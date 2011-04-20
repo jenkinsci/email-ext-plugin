1ff10614009b8c6ee981386701c78f1a8c3a3d9b
slide <slide.o.mix@gmail.com>
2011-04-20 05:38:11 -0700

    Added isPluginInstalled helper method to allow people to check for plugins from scripts generating emails.

--- src/main/resources/hudson/plugins/emailext/templates/groovy/init.groovy
+++ src/main/resources/hudson/plugins/emailext/templates/groovy/init.groovy
@@ -1,5 +1,16 @@
 // just a few helper methods
 
-def min(a, b) {
-	return a < b ? a : b;
+/**
+ * Checks if a plugin is installed based on it's short name.
+ *
+ * @param shortName the short name of the plugin to check for
+ * @returns true if the plugin is installed, false otherwise.
+ */
+boolean isPluginInstalled(String shortName) {
+	boolean result = false;
+	def instance = hudson.model.Hudson.instance;
+	if(instance != null) {
+		result = instance.getPlugin(shortName) != null;
+	}
+	return result;
 }
