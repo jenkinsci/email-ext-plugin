28412f18b43152a8d3b59da490441413eb03b324
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2011-06-30 23:06:19 +0200

    Add option for adding 'Precedence: bulk' header according to http://tools.ietf.org/search/rfc3834.

--- src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
+++ src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
@@ -357,6 +357,10 @@ public class ExtendedEmailPublisher extends Notifier {
             msg.setHeader("List-ID", listId);
         }
 
+        if (ExtendedEmailPublisher.DESCRIPTOR.getPrecedenceBulk()) {
+            msg.setHeader("Precedence", "bulk");
+        }
+
         return msg;
     }
 
