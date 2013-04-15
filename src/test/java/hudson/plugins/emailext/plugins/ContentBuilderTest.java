88136e7e5480fd8df25de171b232761399dfba86
Alex Earl <slide.o.mix@gmail.com>
2013-04-15 07:43:00 -0700

    Fix mocking of AbstractBuild

--- src/test/java/hudson/plugins/emailext/plugins/ContentBuilderTest.java
+++ src/test/java/hudson/plugins/emailext/plugins/ContentBuilderTest.java
@@ -1,13 +1,11 @@
 package hudson.plugins.emailext.plugins;
 
+import hudson.EnvVars;
 import hudson.model.AbstractBuild;
-import hudson.model.FreeStyleBuild;
-import hudson.model.FreeStyleProject;
 import hudson.plugins.emailext.ExtendedEmailPublisher;
 import hudson.plugins.emailext.ExtendedEmailPublisherDescriptor;
 import hudson.util.StreamTaskListener;
 import org.jvnet.hudson.test.HudsonTestCase;
-import org.jvnet.hudson.test.recipes.WithPlugin;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -20,6 +18,7 @@ public class ContentBuilderTest
 {
     private ExtendedEmailPublisher publisher;
     private StreamTaskListener listener;
+    private AbstractBuild<?, ?> build;
 
     @Override
     public void setUp()
@@ -44,24 +43,27 @@ public class ContentBuilderTest
         f = ExtendedEmailPublisherDescriptor.class.getDeclaredField( "recipientList" );
         f.setAccessible( true );
         f.set( ExtendedEmailPublisher.DESCRIPTOR, "ashlux@gmail.com" );
+        
+        build = mock(AbstractBuild.class);
+        when(build.getEnvironment(listener)).thenReturn(new EnvVars());
     }
 
     public void testTransformText_shouldExpand_$PROJECT_DEFAULT_CONTENT()
         throws IOException, InterruptedException
     {
         assertEquals(publisher.defaultContent, new ContentBuilder().transformText( "$PROJECT_DEFAULT_CONTENT", publisher, null,
-                                                         mock( AbstractBuild.class ), listener ));
+                                                         build, listener ));
         assertEquals(publisher.defaultContent, new ContentBuilder().transformText( "${PROJECT_DEFAULT_CONTENT}", publisher, null,
-                                                         mock( AbstractBuild.class ), listener ));
+                                                         build, listener ));
     }
 
     public void testTransformText_shouldExpand_$PROJECT_DEFAULT_SUBJECT()
         throws IOException, InterruptedException
     {
         assertEquals(publisher.defaultSubject, new ContentBuilder().transformText( "$PROJECT_DEFAULT_SUBJECT", publisher, null,
-                                                         mock( AbstractBuild.class ), listener ));
+                                                         build, listener ));
         assertEquals(publisher.defaultSubject, new ContentBuilder().transformText( "${PROJECT_DEFAULT_SUBJECT}", publisher, null,
-                                                         mock( AbstractBuild.class ), listener ));
+                                                         build, listener ));
     }
     
     
@@ -70,10 +72,10 @@ public class ContentBuilderTest
     {
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultBody(),
                         new ContentBuilder().transformText( "$DEFAULT_CONTENT", publisher, null,
-                                                            mock( AbstractBuild.class ), listener ) );
+                                                            build, listener ) );
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultBody(),
                         new ContentBuilder().transformText( "${DEFAULT_CONTENT}", publisher, null,
-                                                            mock( AbstractBuild.class ), listener ) );
+                                                            build, listener ) );
     }
 
     public void testTransformText_shouldExpand_$DEFAULT_SUBJECT()
@@ -81,10 +83,10 @@ public class ContentBuilderTest
     {
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultSubject(),
                         new ContentBuilder().transformText( "$DEFAULT_SUBJECT", publisher, null,
-                                                            mock( AbstractBuild.class ), listener ) );
+                                                            build, listener ) );
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultSubject(),
                         new ContentBuilder().transformText( "${DEFAULT_SUBJECT}", publisher, null,
-                                                            mock( AbstractBuild.class ), listener ) );
+                                                            build, listener ) );
     }
     
     public void testTransformText_shouldExpand_$DEFAULT_RECIPIENT_LIST()
@@ -92,10 +94,10 @@ public class ContentBuilderTest
     {
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultRecipients(),
                         new ContentBuilder().transformText( "$DEFAULT_RECIPIENTS", publisher, null,
-                                                        mock( AbstractBuild.class ), listener ) );
+                                                        build, listener ) );
         assertEquals( ExtendedEmailPublisher.DESCRIPTOR.getDefaultRecipients(),
                         new ContentBuilder().transformText( "${DEFAULT_RECIPIENTS}", publisher, null,
-                                                        mock( AbstractBuild.class ), listener ) );
+                                                        build, listener ) );
     }
     
     public void testTransformText_noNPEWithNullDefaultSubjectBody() throws NoSuchFieldException, IllegalAccessException
@@ -106,7 +108,7 @@ public class ContentBuilderTest
         f = ExtendedEmailPublisherDescriptor.class.getDeclaredField( "defaultSubject" );
         f.setAccessible( true );
         f.set( ExtendedEmailPublisher.DESCRIPTOR, null );
-        assertEquals( "", new ContentBuilder().transformText( "$DEFAULT_SUBJECT", publisher, null, mock (AbstractBuild.class ), listener));
-        assertEquals( "", new ContentBuilder().transformText( "$DEFAULT_CONTENT", publisher, null, mock (AbstractBuild.class ), listener));
+        assertEquals( "", new ContentBuilder().transformText( "$DEFAULT_SUBJECT", publisher, null, build, listener));
+        assertEquals( "", new ContentBuilder().transformText( "$DEFAULT_CONTENT", publisher, null, build, listener));
     }
 }
