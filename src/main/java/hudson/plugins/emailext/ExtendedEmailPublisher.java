d0d8ba054051989a8024b0373c2365dca3e2afea
Alex Earl <slide.o.mix@gmail.com>
2013-04-30 10:59:36 -0700

    Remove extra call to isExcludedCommitter
    Fix merge issue on the double isExcludedCommitter call.

--- src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
+++ src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
@@ -531,7 +531,7 @@ public class ExtendedEmailPublisher extends Notifier implements MatrixAggregatab
             for (User user : users) {
                 if (!isExcludedCommitter(user.getFullName())) {
                     String userAddress = EmailRecipientUtils.getUserConfiguredEmail(user);
-                    if (userAddress != null && !isExcludedCommitter(user.getFullName())) {
+                    if (userAddress != null) {
                         addAddressesFromRecipientList(recipientAddresses, ccAddresses, userAddress, env, listener);
                     } else {
                         listener.getLogger().println("Failed to send e-mail to " + user.getFullName() + " because no e-mail address is known, and no default e-mail domain is configured");
