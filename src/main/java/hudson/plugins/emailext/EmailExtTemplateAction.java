9a66978168285d44f2dd9803f5d5a2799fd78680
Alex Earl <slide.o.mix@gmail.com>
2013-05-20 06:28:56 -0700

    Add support for jelly template testing

--- src/main/java/hudson/plugins/emailext/EmailExtTemplateAction.java
+++ src/main/java/hudson/plugins/emailext/EmailExtTemplateAction.java
@@ -8,6 +8,7 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.TaskListener;
+import hudson.plugins.emailext.plugins.content.JellyScriptContent;
 import hudson.plugins.emailext.plugins.content.ScriptContent;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -40,7 +41,12 @@ public class EmailExtTemplateAction implements Action {
     }   
     
     private String renderError(Exception ex) {
-        return "";
+        StringBuilder builder = new StringBuilder();
+        builder.append("<h3>An error occured trying to render the template:</h3><br/>");
+        builder.append("<span style=\"color:red; font-weight:bold\">");
+        builder.append(ex.toString().replace("\n", "<br/>"));
+        builder.append("</span>");
+        return builder.toString();
     }
     
     @JavaScriptMethod
@@ -48,10 +54,16 @@ public class EmailExtTemplateAction implements Action {
         String result;
         
         try {
-            ScriptContent content = new ScriptContent();
-            content.template = templateFile;
             AbstractBuild<?,?> build = project.getBuild(buildId);
-            result = content.evaluate(build, TaskListener.NULL, "SCRIPT");
+            if(templateFile.endsWith(".jelly")) {
+                JellyScriptContent jellyContent = new JellyScriptContent();
+                jellyContent.template = templateFile;
+                result = jellyContent.evaluate(build, TaskListener.NULL, "JELLY_SCRIPT");
+            } else {
+                ScriptContent scriptContent = new ScriptContent();
+                scriptContent.template = templateFile;                
+                result = scriptContent.evaluate(build, TaskListener.NULL, "SCRIPT");
+            }
         } catch (Exception ex) {
             result = renderError(ex);
         } 
