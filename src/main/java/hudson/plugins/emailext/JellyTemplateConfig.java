9c8e0ac16c440293728a5f7343993857c5f47b00
Alex Earl <slide.o.mix@gmail.com>
2013-10-29 13:40:54 -0700

    Fix JENKINS-18203
    Implement support for managed files as templates.

---
+++ src/main/java/hudson/plugins/emailext/JellyTemplateConfig.java
@@ -0,0 +1,45 @@
+package hudson.plugins.emailext;
+
+import hudson.Extension;
+import org.jenkinsci.lib.configprovider.AbstractConfigProviderImpl;
+import org.jenkinsci.lib.configprovider.model.Config;
+import org.jenkinsci.lib.configprovider.model.ContentType;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+
+public class JellyTemplateConfig extends Config {
+    
+    @DataBoundConstructor
+    public JellyTemplateConfig(String id, String name, String comment, String content) {
+        super(id, name, comment, content);
+    }
+    
+    @Extension
+    public static final class JellyTemplateConfigProvider extends AbstractConfigProviderImpl {
+
+        public JellyTemplateConfigProvider() {
+            //load();
+        }
+
+        @Override
+        public ContentType getContentType() {
+            return ContentType.DefinedType.XML;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Extended Email Publisher Jelly Template ";
+        }
+
+        @Override
+        public Config newConfig() {
+            String id = getProviderId() + System.currentTimeMillis();
+            return new JellyTemplateConfig(id, "Jelly Email Template", "", "");
+        }
+
+        @Override
+        protected String getXmlFileName() {
+            return "email-ext-jelly-config-files.xml";
+        }
+    }    
+}
