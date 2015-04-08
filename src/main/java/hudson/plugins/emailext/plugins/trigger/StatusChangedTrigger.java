8d84b3a2a5634e10e29014f4493cad191698da23
doino.gretchenliev <doino.gretchenliev@ontotext.com>
2015-04-08 19:36:36 +0300

    Add status changed and unstable triggers to default configuration.

--- src/main/java/hudson/plugins/emailext/plugins/trigger/StatusChangedTrigger.java
+++ src/main/java/hudson/plugins/emailext/plugins/trigger/StatusChangedTrigger.java
@@ -1,29 +1,38 @@
 package hudson.plugins.emailext.plugins.trigger;
 
 import hudson.Extension;
+import hudson.model.AbstractBuild;
 import hudson.model.Result;
 import hudson.model.TaskListener;
-import hudson.model.AbstractBuild;
 import hudson.plugins.emailext.ExtendedEmailPublisher;
-import hudson.plugins.emailext.plugins.EmailTriggerDescriptor;
 import hudson.plugins.emailext.plugins.EmailTrigger;
+import hudson.plugins.emailext.plugins.EmailTriggerDescriptor;
 import hudson.plugins.emailext.plugins.RecipientProvider;
 import hudson.plugins.emailext.plugins.recipients.DevelopersRecipientProvider;
-import java.util.List;
-
+import hudson.plugins.emailext.plugins.recipients.ListRecipientProvider;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.Collections;
+import java.util.List;
 
 public class StatusChangedTrigger extends EmailTrigger {
 
     public static final String TRIGGER_NAME = "Status Changed";
 
+    public static StatusChangedTrigger createDefault() {
+        return new StatusChangedTrigger(Collections.<RecipientProvider>singletonList(new ListRecipientProvider()), "", "$PROJECT_DEFAULT_REPLYTO", "$PROJECT_DEFAULT_SUBJECT", "$PROJECT_DEFAULT_CONTENT", "", 0, "project");
+    }
+
     @DataBoundConstructor
-    public StatusChangedTrigger(List<RecipientProvider> recipientProviders, String recipientList, String replyTo, String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
+    public StatusChangedTrigger(List<RecipientProvider> recipientProviders, String recipientList, String replyTo,
+        String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
         super(recipientProviders, recipientList, replyTo, subject, body, attachmentsPattern, attachBuildLog, contentType);
     }
     
     @Deprecated
-    public StatusChangedTrigger(boolean sendToList, boolean sendToDevs, boolean sendToRequester, boolean sendToCulprits, String recipientList, String replyTo, String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
+    public StatusChangedTrigger(boolean sendToList, boolean sendToDevs, boolean sendToRequester, boolean sendToCulprits,
+        String recipientList, String replyTo, String subject, String body, String attachmentsPattern,
+        int attachBuildLog, String contentType) {
         super(sendToList, sendToDevs, sendToRequester, sendToCulprits,recipientList, replyTo, subject, body, attachmentsPattern, attachBuildLog, contentType);
     }
 
--- src/main/java/hudson/plugins/emailext/plugins/trigger/UnstableTrigger.java
+++ src/main/java/hudson/plugins/emailext/plugins/trigger/UnstableTrigger.java
@@ -8,21 +8,30 @@ import hudson.plugins.emailext.plugins.EmailTrigger;
 import hudson.plugins.emailext.plugins.EmailTriggerDescriptor;
 import hudson.plugins.emailext.plugins.RecipientProvider;
 import hudson.plugins.emailext.plugins.recipients.DevelopersRecipientProvider;
-import java.util.List;
-
+import hudson.plugins.emailext.plugins.recipients.ListRecipientProvider;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.Collections;
+import java.util.List;
 
 public class UnstableTrigger extends EmailTrigger {
 
     public static final String TRIGGER_NAME = "Unstable (Test Failures)";
-    
+
+    public static UnstableTrigger createDefault() {
+        return new UnstableTrigger(Collections.<RecipientProvider>singletonList(new ListRecipientProvider()), "", "$PROJECT_DEFAULT_REPLYTO", "$PROJECT_DEFAULT_SUBJECT", "$PROJECT_DEFAULT_CONTENT", "", 0, "project");
+    }
+
     @DataBoundConstructor
-    public UnstableTrigger(List<RecipientProvider> recipientProviders, String recipientList, String replyTo, String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
+    public UnstableTrigger(List<RecipientProvider> recipientProviders, String recipientList, String replyTo,
+        String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
         super(recipientProviders, recipientList, replyTo, subject, body, attachmentsPattern, attachBuildLog, contentType);
     }
     
     @Deprecated
-    public UnstableTrigger(boolean sendToList, boolean sendToDevs, boolean sendToRequester, boolean sendToCulprits, String recipientList, String replyTo, String subject, String body, String attachmentsPattern, int attachBuildLog, String contentType) {
+    public UnstableTrigger(boolean sendToList, boolean sendToDevs, boolean sendToRequester, boolean sendToCulprits,
+        String recipientList, String replyTo, String subject, String body, String attachmentsPattern,
+        int attachBuildLog, String contentType) {
         super(sendToList, sendToDevs, sendToRequester, sendToCulprits,recipientList, replyTo, subject, body, attachmentsPattern, attachBuildLog, contentType);
     }
 
