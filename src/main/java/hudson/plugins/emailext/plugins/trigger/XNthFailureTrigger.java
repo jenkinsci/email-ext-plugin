5c4db3bacf38a8d33260b61237634a337292b871
Kanstantsin Shautsou <kanstantsin.sha@gmail.com>
2016-10-09 03:35:54 +0300

    [JENKINS-37995] Fix getter value in UI
    Signed-off-by: Kanstantsin Shautsou <kanstantsin.sha@gmail.com>

--- src/main/java/hudson/plugins/emailext/plugins/trigger/XNthFailureTrigger.java
+++ src/main/java/hudson/plugins/emailext/plugins/trigger/XNthFailureTrigger.java
@@ -24,7 +24,7 @@ public class XNthFailureTrigger extends NthFailureTrigger {
     }
 
     @Override
-    protected int getRequiredFailureCount() {
+    public int getRequiredFailureCount() {
         return requiredFailureCount;
     }
 
--- src/main/resources/hudson/plugins/emailext/plugins/trigger/XNthFailureTrigger/local-config.groovy
+++ src/main/resources/hudson/plugins/emailext/plugins/trigger/XNthFailureTrigger/local-config.groovy
@@ -2,6 +2,6 @@ package hudson.plugins.emailext.plugins.trigger.XNthFailureTrigger
 
 f = namespace("/lib/form")
 
-f.entry(title: _("Number Of Failures")) {
-    f.number(name: "requiredFailureCount")
+f.entry(title: _("Number Of Failures"), field: "requiredFailureCount") {
+    f.number()
 }
