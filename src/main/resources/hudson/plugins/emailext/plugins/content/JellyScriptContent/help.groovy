9012fcd1b9b2040f2d34c2723198a751eb81cce5
Josh Soref <jsoref@users.noreply.github.com>
2018-05-28 03:45:28 -0400

    Help markup (#169)
    * help: markup variables with tt
    
    * help: markup env variables with tt
    
    * help: markup objects with em
    
    * spelling: paths
    
    * help: link jira issue
    
    * help: tt input/matches for email
    
    * help: markup actions with em
    
    * help: markup input with tt
    
    * help: fix closing anchor tag
    
    * help: markup smtp header with tt
    
    * help: markup variables with tt
    
    * help: period at end of sentence
    
    * help: markup smtp header with tt
    
    * help: markup variables with tt
    
    * help: markup variables with tt/strong/em
    
    * brand: Groovy
    
    * grammar: whether or not
    
    * grammar x and y foo-s
    
    * alternate language
    
    * trailing whitespace
    
    * brand: Groovy
    
    * help: fix missing space
    
    * help: markup variables/input with tt
    
    * help: parenthesize example
    
    It isn't a sentence, so doesn't deserve a period...
    
    * help: fix missing space
    
    * help: markup variables/input with tt
    
    * help: parenthesize example

--- src/main/resources/hudson/plugins/emailext/plugins/content/JellyScriptContent/help.groovy
+++ src/main/resources/hudson/plugins/emailext/plugins/content/JellyScriptContent/help.groovy
@@ -1,9 +1,9 @@
 dt("\${JELLY_SCRIPT,template=\"TEMPLATE_NAME\"}")
 dd(_("Custom message content generated from a Jelly script template. "
     +"There are two templates provided: \"html\" and \"text\". "
-    +"Custom Jelly templates should be placed in \$JENKINS_HOME/email-templates."
+    +"Custom Jelly templates should be placed in <tt>\$JENKINS_HOME/email-templates</tt>. "
     +"When using custom templates, the template filename without \".jelly\" "
-    +"should be used for the \"template\" argument."
+    +"should be used for the \"template\" argument. "
     +"You may also use the Config File Provider plugin to manage your templates. "
     +"Prefix the managed file NAME with \"managed:\" for the template "
-    +"parameter. Example: \${JELLY_SCRIPT, template=\"managed:ManagedFileName\"}"))
+    +"parameter. (Example: <tt>\${JELLY_SCRIPT, template=\"managed:ManagedFileName\"}</tt>)"))
--- src/main/resources/hudson/plugins/emailext/plugins/content/ScriptContent/help.groovy
+++ src/main/resources/hudson/plugins/emailext/plugins/content/ScriptContent/help.groovy
@@ -1,18 +1,18 @@
 dt("\${SCRIPT}")
 dd() {
-  span(_("Custom message content generated from a groovy script." +
-     "Custom scripts should be placed in \"\$JENKINS_HOME/email-templates\". "+
+  span(_("Custom message content generated from a Groovy script. " +
+     "Custom scripts should be placed in <tt>\$JENKINS_HOME/email-templates</tt>. "+
      "When using a custom script the plugin will look in the resources for" +
-     "the email-ext plugin first, and then in the \$JENKINS_HOME/email-templates" + 
-     "directory. No other directories will be searched." +
+     "the email-ext plugin first, and then in the <tt>\$JENKINS_HOME/email-templates</tt>" +
+     "directory. No other directories will be searched. " +
      "You may also use the Config File Provider plugin to manage your scripts. " +
-     "Prefix the managed file NAME with \"managed:\" for the template or script " +
-     "parameter. Example: \${SCRIPT, template=\"managed:ManagedFileName\"}"))
-  
+     "Prefix the managed file NAME with <tt>managed:</tt> for the template or script " +
+     "parameter. (Example: <tt>\${SCRIPT, template=\"managed:ManagedFileName\"}</tt>)"))
+
   dl() {
     dt("script")
     dd("When this is used, only the last value in the script will be used in the expansion (script and template can not be used together).")
-  
+
     dt("template")
     dd("The template in Groovy's SimpleTemplateEngine format.")
   }
