f02d3b394b3a7d2c43d7fc1b044ba19848cdb084
Josh Soref <jsoref@users.noreply.github.com>
2018-05-28 06:28:22 +0000

    help: markup variables/input with tt

--- src/main/resources/hudson/plugins/emailext/plugins/content/TemplateContent/help.groovy
+++ src/main/resources/hudson/plugins/emailext/plugins/content/TemplateContent/help.groovy
@@ -1,14 +1,14 @@
 dt("\${TEMPLATE}")
 dd() {
   span(_("Custom message content generated from a template file. " +
-     "Custom templates should be placed in \"\$JENKINS_HOME/email-templates\". "+
+     "Custom templates should be placed in <tt>\$JENKINS_HOME/email-templates</tt>. "+
      "When using a custom script the plugin will look in the resources for" +
-     "the email-ext plugin first, and then in the \$JENKINS_HOME/email-templates" + 
+     "the email-ext plugin first, and then in the <tt>\$JENKINS_HOME/email-templates</tt>" + 
      "directory. No other directories will be searched. " +
      "You may also use the Config File Provider plugin to manage your templates. " +
      "Use the \"Custom File\" config type for the template. " + 
      "Prefix the managed file NAME with \"managed:\" for the file " +
-     "parameter. Example: \${TEMPLATE, file=\"managed:ManagedFileName\"}"))
+     "parameter. Example: <tt>\${TEMPLATE, file=\"managed:ManagedFileName\"}</tt>"))
   
   dl() {
     dt("file")
