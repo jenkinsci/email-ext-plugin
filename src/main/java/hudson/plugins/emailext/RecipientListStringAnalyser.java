e94eddc7d06ba8ee631a7edddaba8e45287fc984
Reinhold Füreder <r.fuereder@xortex.com>
2018-07-31 11:23:42 +0200

    Implement new RecipientListStringAnalyser algorithm to actually fix this issue and keep recipients
    - New algorithm is based on getType() being called in order of parsed InternetAddresses; it remembers last found index and so analyses the recipients list string also from left to right (whereas the left starting point is moving to the right)
    - Still with sysout debug output => TODO: convert to debug logging

--- src/main/java/hudson/plugins/emailext/RecipientListStringAnalyser.java
+++ src/main/java/hudson/plugins/emailext/RecipientListStringAnalyser.java
@@ -2,7 +2,7 @@ package hudson.plugins.emailext;
 
 import javax.mail.internet.InternetAddress;
 
-public class RecipientListStringAnalyser {
+class RecipientListStringAnalyser {
 
     static final int NOT_FOUND = -1;
 
@@ -10,29 +10,84 @@ public class RecipientListStringAnalyser {
 
     private int idx = 0;
 
-    public RecipientListStringAnalyser(String recipientsListString) {
+    RecipientListStringAnalyser(String recipientsListString) {
         this.recipients = recipientsListString;
+        System.out.println("Analyzing:");
+        System.out.println(recipients);
     }
 
-    public int getType(InternetAddress address) {
+    /**
+     * Mind that this method must be called in order of the found InternetAddresses (from parsing the recipients list
+     * string)!
+     */
+    int getType(InternetAddress address) {
         int type = NOT_FOUND;
-        if (address.getPersonal() != null) {
-            if (recipients.contains("bcc:" + address.getPersonal()) || recipients.contains("bcc:\"" + address.toString() + "\"")) {
-                type = EmailRecipientUtils.BCC;
-            } else if (recipients.contains("cc:" + address.getPersonal()) || recipients.contains("cc:\"" + address.toString() + "\"")) {
-                type = EmailRecipientUtils.CC;
-            } else if (recipients.contains(address.getPersonal()) || recipients.contains("\"" + address.toString() + "\"")) {
-                type = EmailRecipientUtils.TO;
-            }
-        } else {
-            if(recipients.contains("bcc:" + address.toString())) {
-                type = EmailRecipientUtils.BCC;
-            } else if(recipients.contains("cc:" + address.toString())) {
-                type = EmailRecipientUtils.CC;
-            } else if(recipients.contains(address.toString())) {
-                type = EmailRecipientUtils.TO;
-            }
+        System.out.println("Looking for: " + address);
+        System.out.println("...starting at: " + idx);
+        int firstFoundIdx = findFirst(address);
+        System.out.println("firstFoundIdx: " + firstFoundIdx);
+        if (firstFoundIdx != Integer.MAX_VALUE) {
+            System.out.println("firstFoundIdx-substring: " + recipients.substring(firstFoundIdx));
+            type = getType(firstFoundIdx);
+            idx += address.toString().length() + 1 + lengthOfTypePrefix(type) + adaptLengthForOptionalPersonal(address);
         }
         return type;
     }
+
+    private int findFirst(InternetAddress address) {
+        int firstIdx = Integer.MAX_VALUE;
+        if (address.getPersonal() != null) {
+            firstIdx = findFirst(firstIdx, "bcc:" + address.getPersonal());
+            firstIdx = findFirst(firstIdx, "bcc:\"" + address.toString() + "\"");
+            firstIdx = findFirst(firstIdx, "cc:" + address.getPersonal());
+            firstIdx = findFirst(firstIdx, "cc:\"" + address.toString() + "\"");
+            firstIdx = findFirst(firstIdx, address.getPersonal());
+            firstIdx = findFirst(firstIdx, "\"" + address.toString() + "\"");
+        } else {
+            firstIdx = findFirst(firstIdx, "bcc:" + address.toString());
+            firstIdx = findFirst(firstIdx, "cc:" + address.toString());
+            firstIdx = findFirst(firstIdx, address.toString());
+        }
+        return firstIdx;
+    }
+
+    private int findFirst(int firstIdx, String search) {
+        int foundIdx = recipients.indexOf(search, idx);
+        return (foundIdx == -1) ? firstIdx : Math.min(firstIdx, foundIdx);
+    }
+
+    private int getType(int firstFoundIdx) {
+        int type;
+        if (recipients.indexOf("bcc", firstFoundIdx) == firstFoundIdx) {
+            type = EmailRecipientUtils.BCC;
+        } else if (recipients.indexOf("cc", firstFoundIdx) == firstFoundIdx) {
+            type = EmailRecipientUtils.CC;
+        } else {
+            type = EmailRecipientUtils.TO;
+        }
+        return type;
+    }
+
+    private int lengthOfTypePrefix(int type) {
+        int length;
+        switch (type) {
+            case EmailRecipientUtils.BCC:
+                length = 3;
+                break;
+            case EmailRecipientUtils.CC:
+                length = 2;
+                break;
+            case EmailRecipientUtils.TO:
+                length = 0;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown type prefix: " + type);
+        }
+        return length;
+    }
+
+    private int adaptLengthForOptionalPersonal(InternetAddress address) {
+        return address.getPersonal() != null ? -4 : 0; // This may not be precise! Quotes around personal name + '<' and '>' around email address...
+    }
+
 }
--- src/test/java/hudson/plugins/emailext/RecipientListStringAnalyserTest.java
+++ src/test/java/hudson/plugins/emailext/RecipientListStringAnalyserTest.java
@@ -54,74 +54,111 @@ public class RecipientListStringAnalyserTest {
     }
 
     @Test
-    public void getTypeForPartiallyCcPrefixedEmailAddresses() throws Exception {
-        String[] testStrings = {
-                "mickey@disney.com, cc:donald@disney.com",
-                "cc:donald@disney.com, mickey@disney.com"
-        };
-        for(String testString : testStrings) {
-            RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(testString);
-            assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
-            assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
-        }
+    public void getTypeForPartiallyCcPrefixed2ndEmailAddresses() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("mickey@disney.com, cc:donald@disney.com");
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcPrefixed1stEmailAddresses() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("cc:donald@disney.com, mickey@disney.com");
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
     }
 
     @Test
-    public void getTypeForPartiallyBccPrefixedEmailAddresses() throws Exception {
-        String[] testStrings = {
-                "mickey@disney.com, bcc:donald@disney.com",
-                "bcc:donald@disney.com, mickey@disney.com"
-        };
-        for(String testString : testStrings) {
-            RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(testString);
-            assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
-            assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
-        }
+    public void getTypeForPartiallyBccPrefixed2ndEmailAddresses() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("mickey@disney.com, bcc:donald@disney.com");
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyBccPrefixed1stEmailAddresses() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("bcc:donald@disney.com, mickey@disney.com");
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
     }
 
     @Test
     public void getTypeForCcAndBccPrefixedEmailAddresses() throws Exception {
-        String[] testStrings = {
-                "cc:mickey@disney.com, bcc:donald@disney.com",
-                "bcc:donald@disney.com, cc:mickey@disney.com"
-        };
-        for(String testString : testStrings) {
-            RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(testString);
-            assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("mickey@disney.com")));
-            assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
-        }
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("cc:mickey@disney.com, bcc:donald@disney.com");
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
+    }
+    @Test
+    public void getTypeForBccAndCcPrefixedEmailAddresses() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("bcc:donald@disney.com, cc:mickey@disney.com");
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("mickey@disney.com")));
     }
 
+    /*
+     * Calls to {@link RecipientListStringAnalyser#getType(InternetAddress)} must be in right order; in order to avoid
+     * bugs in test case logic each permutation is implemented (again) in its own test case below.
+     */
     @Test
-    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses() throws Exception {
-        String[] testStrings = {
-                "mickey@disney.com, cc:donald@disney.com, bcc:goofy@disney.com",
-                "mickey@disney.com, bcc:goofy@disney.com, cc:donald@disney.com",
-                "cc:donald@disney.com, mickey@disney.com, bcc:goofy@disney.com",
-                "cc:donald@disney.com, bcc:goofy@disney.com, mickey@disney.com",
-                "bcc:goofy@disney.com, mickey@disney.com, cc:donald@disney.com",
-                "bcc:goofy@disney.com, cc:donald@disney.com, mickey@disney.com"
-        };
-        for(String testString : testStrings) {
-            RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(testString);
-            assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
-            assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
-            assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
-        }
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses1() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "mickey@disney.com, cc:donald@disney.com, bcc:goofy@disney.com");
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses2() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "mickey@disney.com, bcc:goofy@disney.com, cc:donald@disney.com");
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses3() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "cc:donald@disney.com, mickey@disney.com, bcc:goofy@disney.com");
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses4() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "cc:donald@disney.com, bcc:goofy@disney.com, mickey@disney.com");
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses5() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "bcc:goofy@disney.com, mickey@disney.com, cc:donald@disney.com");
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+    }
+    @Test
+    public void getTypeForPartiallyCcAndBccPrefixedEmailAddresses6() throws Exception {
+        RecipientListStringAnalyser analyser = new RecipientListStringAnalyser(
+                "bcc:goofy@disney.com, cc:donald@disney.com, mickey@disney.com");
+        assertEquals(EmailRecipientUtils.BCC, analyser.getType(new InternetAddress("goofy@disney.com")));
+        assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("donald@disney.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
     }
 
     @Test
     public void testUTF8() throws Exception {
         RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("ashlux@gmail.com, cc:slide.o.mix@gmail.com, 愛嬋 <another@gmail.com>");
         assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("ashlux@gmail.com")));
-        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("another@gmail.com")));
         assertEquals(EmailRecipientUtils.CC, analyser.getType(new InternetAddress("slide.o.mix@gmail.com")));
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("another@gmail.com")));
     }
 
     @Test
     public void testBackwardsNames() throws Exception {
         RecipientListStringAnalyser analyser = new RecipientListStringAnalyser("\"Mouse, Mickey\" <mickey@disney.com>, minnie@disney.com");
-        assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("mickey@disney.com")));
+        InternetAddress addressWithPersonal = new InternetAddress("mickey@disney.com");
+        addressWithPersonal.setPersonal("\"Mouse, Mickey\"");
+        assertEquals(EmailRecipientUtils.TO, analyser.getType(addressWithPersonal));
         assertEquals(EmailRecipientUtils.TO, analyser.getType(new InternetAddress("minnie@disney.com")));
     }
 
