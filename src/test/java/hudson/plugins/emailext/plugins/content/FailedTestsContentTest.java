f1e55fdd98b5f986c3ec96f379a52417357bb1b3
Divyesh Chandra <divyesh1234@gmail.com>
2020-05-14 03:53:52 +0530

    Remove * imports

--- src/test/java/hudson/plugins/emailext/plugins/content/FailedTestsContentTest.java
+++ src/test/java/hudson/plugins/emailext/plugins/content/FailedTestsContentTest.java
@@ -19,7 +19,9 @@ import java.util.List;
 
 import static java.util.Collections.singletonList;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
