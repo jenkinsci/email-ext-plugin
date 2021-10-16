7d21560f8ecf138bcd80abb9e9b22013c03e70a7
NotMyFault <mc.cache@web.de>
2021-10-16 23:55:19 +0200

    feature: Use modernized icons (#318)

--- src/main/resources/hudson/plugins/emailext/EmailExtTemplateAction/action.groovy
+++ src/main/resources/hudson/plugins/emailext/EmailExtTemplateAction/action.groovy
@@ -4,6 +4,6 @@ def requiresAdmin = app.getDescriptor('hudson.plugins.emailext.ExtendedEmailPubl
 def hasPermission = requiresAdmin ? h.hasPermission(app.MANAGE) : h.hasPermission(this.action.project, this.action.project.CONFIGURE)
 
 if(hasPermission) {
-    l.task(icon: "/plugin/email-ext/images/template-debugger.png", title: this.action.displayName, 
+    l.task(icon: "/plugin/email-ext/images/template-debugger.svg", title: this.action.displayName,
         href: h.getActionUrl(my.url, this.action), contextMenu: h.isContextMenuVisible(this.action))
 }
