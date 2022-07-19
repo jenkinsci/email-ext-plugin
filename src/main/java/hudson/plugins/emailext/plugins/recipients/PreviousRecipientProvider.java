1f59b9277a06e57968b083be6495010ea874016d
Basil Crow <me@basilcrow.com>
2022-07-19 10:37:51 -0700

    Fix imports

--- src/main/java/hudson/plugins/emailext/plugins/recipients/PreviousRecipientProvider.java
+++ src/main/java/hudson/plugins/emailext/plugins/recipients/PreviousRecipientProvider.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.mail.internet.InternetAddress;
+import jakarta.mail.internet.InternetAddress;
 
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
