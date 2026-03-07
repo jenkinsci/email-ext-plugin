148f99426a17367847783208813cccd54ef9da09
Akash Manna <akash.manna.mymail@gmail.com>
2026-03-08 00:48:57 +0530

    Refactor template rendering to use fetch API and improve CSP compliance

--- src/test/java/hudson/plugins/emailext/EmailExtTemplateActionTest.java
+++ src/test/java/hudson/plugins/emailext/EmailExtTemplateActionTest.java
@@ -9,6 +9,8 @@ import org.jvnet.hudson.test.Issue;
 
 /**
  * Test for EmailExtTemplateAction CSP compliance.
+ * 
+ * @Author Akash Manna
  */
 public class EmailExtTemplateActionTest {
 
@@ -18,13 +20,35 @@ public class EmailExtTemplateActionTest {
         java.io.InputStream is = getClass()
                 .getClassLoader()
                 .getResourceAsStream("hudson/plugins/emailext/EmailExtTemplateAction/template-test.js");
-        assertNotNull(is);
+        assertNotNull(is, "JavaScript file should exist");
 
         String jsContent = new String(is.readAllBytes(), java.nio.charset.StandardCharsets.UTF_8);
 
-        assertFalse(jsContent.contains("innerHTML"));
-        assertFalse(jsContent.contains("escape("));
-        assertTrue(jsContent.contains("textContent"));
-        assertTrue(jsContent.contains("encodeURIComponent"));
+        assertFalse(jsContent.contains("innerHTML"), "JavaScript should not use innerHTML (CSP violation)");
+        assertFalse(jsContent.contains("escape("), "JavaScript should not use deprecated escape() function");
+
+        assertTrue(jsContent.contains("textContent"), "JavaScript should use textContent instead of innerHTML");
+        assertTrue(
+                jsContent.contains("encodeURIComponent"), "JavaScript should use encodeURIComponent instead of escape");
+
+        assertTrue(jsContent.contains("fetch("), "JavaScript should use fetch API for AJAX calls");
+        assertFalse(
+                jsContent.contains("templateTester.renderTemplate"),
+                "JavaScript should not use JavaScriptMethod binding (violates CSP)");
+    }
+
+    @Test
+    @Issue("JENKINS-74891")
+    public void testGroovyTemplateDoesNotUseStaplerBind() throws Exception {
+        java.io.InputStream is = getClass()
+                .getClassLoader()
+                .getResourceAsStream("hudson/plugins/emailext/EmailExtTemplateAction/index.groovy");
+        assertNotNull(is, "Groovy template should exist");
+
+        String groovyContent = new String(is.readAllBytes(), java.nio.charset.StandardCharsets.UTF_8);
+
+        assertFalse(
+                groovyContent.contains("st.bind"),
+                "Groovy template should not use st.bind (generates inline JavaScript violating CSP)");
     }
 }
