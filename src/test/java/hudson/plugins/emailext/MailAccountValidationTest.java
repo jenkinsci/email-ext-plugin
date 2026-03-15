cc7b91f4d29bd3b6062d87394c9d757f72477f48
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-15 23:54:55 +0530

    Use JUnit 5 imports in MailAccountValidation

--- src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
+++ src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
@@ -1,25 +1,25 @@
 package hudson.plugins.emailext;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MailAccountValidationTest {
 
     @Test
-    public void testInvalidHighSmtpPort() {
-        int port = 70000; // greater than allowed range
-        assertTrue("SMTP port should be invalid if greater than 65535", port > 65535);
+    void testInvalidHighSmtpPort() {
+        int port = 70000;
+        assertTrue(port > 65535, "SMTP port should be invalid if greater than 65535");
     }
 
     @Test
-    public void testInvalidLowSmtpPort() {
-        int port = -1; // lower than allowed range
-        assertTrue("SMTP port should be invalid if less than 1", port < 1);
+    void testInvalidLowSmtpPort() {
+        int port = -1;
+        assertTrue(port < 1, "SMTP port should be invalid if less than 1");
     }
 
     @Test
-    public void testValidSmtpPort() {
-        int port = 25; // valid SMTP port
-        assertTrue("SMTP port should be valid if between 1 and 65535", port > 0 && port <= 65535);
+    void testValidSmtpPort() {
+        int port = 25;
+        assertTrue(port > 0 && port <= 65535, "SMTP port should be valid if between 1 and 65535");
     }
 }
