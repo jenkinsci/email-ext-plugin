6cf793a04738fee1487133147ae10c5eb0513310
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-15 23:57:24 +0530

    Fix MailAccountValidationTest to test mailaccount class

--- src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
+++ src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
@@ -7,19 +7,25 @@ public class MailAccountValidationTest {
 
     @Test
     void testInvalidHighSmtpPort() {
-        int port = 70000;
-        assertTrue(port > 65535, "SMTP port should be invalid if greater than 65535");
+        MailAccount account = new MailAccount();
+        account.setSmtpPort("70000");
+
+        assertEquals("70000", account.getSmtpPort());
     }
 
     @Test
     void testInvalidLowSmtpPort() {
-        int port = -1;
-        assertTrue(port < 1, "SMTP port should be invalid if less than 1");
+        MailAccount account = new MailAccount();
+        account.setSmtpPort("-1");
+
+        assertEquals("-1", account.getSmtpPort());
     }
 
     @Test
     void testValidSmtpPort() {
-        int port = 25;
-        assertTrue(port > 0 && port <= 65535, "SMTP port should be valid if between 1 and 65535");
+        MailAccount account = new MailAccount();
+        account.setSmtpPort("25");
+
+        assertEquals("25", account.getSmtpPort());
     }
 }
