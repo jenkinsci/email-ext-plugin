905624c8e40b5ed8229110027fb823a280361bbf
Arpan Chakraborty <chakrabortyarpan224@gmail.com>
2026-03-15 11:10:21 +0530

    fix: apply spotless formatting

--- src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
+++ src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
@@ -858,14 +858,14 @@ public class ExtendedEmailPublisher extends Notifier {
         boolean useSecurity = Jenkins.get().isUseSecurity();
         if (!classpathList.isEmpty()) {
             try (GroovyClassLoader gloader = new GroovyClassLoader(loader)) {
-            gloader.setShouldRecompile(true);
-            for (ClasspathEntry entry : classpathList) {
-                if (useSecurity) {
-                    ScriptApproval.get().using(entry);
+                gloader.setShouldRecompile(true);
+                for (ClasspathEntry entry : classpathList) {
+                    if (useSecurity) {
+                        ScriptApproval.get().using(entry);
+                    }
+                    gloader.addURL(entry.getURL());
                 }
-                gloader.addURL(entry.getURL());
-            }
-            loader = gloader;
+                loader = gloader;
             }
         }
         if (useSecurity) {
