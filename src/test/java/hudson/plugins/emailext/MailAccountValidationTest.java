e03ccb30394ae1d7150be55358bbf71e8128759b
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-16 00:12:40 +0530

    Simplify Mailaccountvalidationtest to test mailacccount behavior

--- src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
+++ src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
@@ -6,26 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MailAccountValidationTest {
 
     @Test
-    void testInvalidHighSmtpPort() {
-        MailAccount account = new MailAccount();
-        account.setSmtpPort("70000");
-
-        assertEquals("70000", account.getSmtpPort());
-    }
-
-    @Test
-    void testInvalidLowSmtpPort() {
-        MailAccount account = new MailAccount();
-        account.setSmtpPort("-1");
-
-        assertEquals("-1", account.getSmtpPort());
-    }
-
-    @Test
-    void testValidSmtpPort() {
+    void testSetSmtpPort() {
         MailAccount account = new MailAccount();
         account.setSmtpPort("25");
-
         assertEquals("25", account.getSmtpPort());
     }
 }
