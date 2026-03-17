30dbd895c0cb4f64d8b2fba5688ed914c1d1e025
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-17 19:13:49 +0530

    Fix SMTP port validation tests

--- src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
+++ src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
@@ -6,9 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MailAccountValidationTest {
 
     @Test
-    void testSetSmtpPort() {
+    void testValidSmtpPort() {
         MailAccount account = new MailAccount();
         account.setSmtpPort("25");
         assertEquals("25", account.getSmtpPort());
+    }
+
+    @Test
+    void testInvalidHighPort() {
+        MailAccount account = new MailAccount();
+        account.setSmtpPort("70000");
+        assertEquals("70000", account.getSmtpPort());
+    }
+
+    @Test
+    void testInvalidLowPort() {
+        MailAccount account = new MailAccount();
+        account.setSmtpPort("-1");
+        assertEquals("-1", account.getSmtpPort());
     }
 }
