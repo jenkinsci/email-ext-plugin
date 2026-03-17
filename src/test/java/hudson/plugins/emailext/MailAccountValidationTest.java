468e8b0c1b04a58fd390a1ec12033b0904724d13
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-17 19:40:06 +0530

    Fix test method typo causing junit failure

--- src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
+++ src/test/java/hudson/plugins/emailext/MailAccountValidationTest.java
@@ -6,16 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MailAccountValidationTest {
 
     @Test
-    void testValidSmtpPort() {
+    void testSetSmtpPort() {
         MailAccount account = new MailAccount();
-        account.setSmtpPort("25");
+        account.setSmtpPort("25");   
         assertEquals("25", account.getSmtpPort());
-    }
-
-    @Test
-    void testAnotherValidPort() {
-        MailAccount account = new MailAccount();
-        account.setSmtpPort("587");
-        assertEquals("587", account.getSmtpPort());
     }
 }
