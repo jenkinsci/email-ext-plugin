ea1f923cbc13117f34b236965b27cc50f0e768d8
Vaibhav Mali <malivaibhav162@gmail.com>
2026-03-22 10:25:13 +0530

    Shorten SMTP logging message to satisfy checkstyle

--- src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
+++ src/main/java/hudson/plugins/emailext/ExtendedEmailPublisher.java
@@ -618,7 +618,7 @@ public class ExtendedEmailPublisher extends Notifier {
                                             || e.getNextException() instanceof ConnectException)) {
                                 context.getListener()
                                         .getLogger()
-                                        .println("SMTP connection failed (Socket/Connect exception). Retrying email send in 10 seconds...");
+                                        .println("SMTP connection failed. Retrying in 10 seconds...");
                                 transport.close();
                                 Thread.sleep(10000);
                             } else {
