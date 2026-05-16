92b4ed1223244339f60b0c95596f51d2d59617d6
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-05-16 12:23:19 +0530

    fix: use Level.INFO for interrupt log and fix test assertion to use isInterrupted()

--- src/main/java/hudson/plugins/emailext/plugins/ContentBuilder.java
+++ src/main/java/hudson/plugins/emailext/plugins/ContentBuilder.java
@@ -94,7 +94,7 @@ public final class ContentBuilder {
             context.getListener().getLogger().println("Error evaluating token: " + e.getMessage());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            Logger.getLogger(ContentBuilder.class.getName()).log(Level.WARNING, "Token expansion interrupted", e);
+            Logger.getLogger(ContentBuilder.class.getName()).log(Level.INFO, "Token expansion interrupted", e);
         } catch (Exception e) {
             Logger.getLogger(ContentBuilder.class.getName()).log(Level.SEVERE, null, e);
         }
