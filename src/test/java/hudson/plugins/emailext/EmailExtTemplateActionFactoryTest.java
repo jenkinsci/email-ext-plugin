73fec7adda4d90f1089ee9f2341dd0646c1cb62b
Akash Manna <akash.manna.mymail@gmail.com>
2026-06-06 22:24:18 +0530

    Use idiomatic Mockito stubbing for null publishers list test

--- src/test/java/hudson/plugins/emailext/EmailExtTemplateActionFactoryTest.java
+++ src/test/java/hudson/plugins/emailext/EmailExtTemplateActionFactoryTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import hudson.model.Action;
 import hudson.model.FreeStyleProject;
-import hudson.util.DescribableList;
 import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -20,6 +19,7 @@ class EmailExtTemplateActionFactoryTest {
     void testCreateForReturnsEmptyListWhenNoEmailExt(JenkinsRule j) throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
         EmailExtTemplateActionFactory factory = new EmailExtTemplateActionFactory();
+
         assertTrue(factory.createFor(project).isEmpty());
     }
 
@@ -27,20 +27,25 @@ class EmailExtTemplateActionFactoryTest {
     void testCreateForReturnsActionWhenEmailExtPresent(JenkinsRule j) throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
         project.getPublishersList().add(new ExtendedEmailPublisher());
+
         EmailExtTemplateActionFactory factory = new EmailExtTemplateActionFactory();
 
         Collection<? extends Action> actions = factory.createFor(project);
 
         assertEquals(1, actions.size());
+
         Action action = actions.iterator().next();
         EmailExtTemplateAction templateAction = assertInstanceOf(EmailExtTemplateAction.class, action);
+
         assertEquals(project, templateAction.getProject());
     }
 
     @Test
     void testCreateForReturnsEmptyListWhenPublishersListIsNull() {
         FreeStyleProject project = Mockito.mock(FreeStyleProject.class);
-        Mockito.doReturn((DescribableList<?, ?>) null).when(project).getPublishersList();
+
+        // Defensive test for a potential contract violation.
+        Mockito.when(project.getPublishersList()).thenReturn(null);
 
         EmailExtTemplateActionFactory factory = new EmailExtTemplateActionFactory();
 
